d1d8c66885390582373927c8cf7e654a3911fddb
s30java <s30java@gmail.com>
2014-04-30 18:26:24 +0530

    comitted files for first setup .

--- src/main/java/com/fas/controller/LoginController.java
+++ src/main/java/com/fas/controller/LoginController.java
@@ -3,10 +3,28 @@
  */
 package com.fas.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 /**
  * @author swaybhase
  *
  */
+
+@Controller
 public class LoginController {
+	
+	
+	@RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+	public ModelAndView getLogin(){
+		ModelAndView modal=new ModelAndView();
+		
+		modal.addObject("message", "Satish");
+		modal.setViewName("login");
+		
+		
+		return modal;
+	}
 
 }
